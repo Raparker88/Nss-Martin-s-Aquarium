6f51aada101feeca342c0c7c9ba517cdc575aacf
Raparker88 <raparker88@gmail.com>
2020-07-14 12:11:02 -0500

    added main.js and imported fish function

---
+++ scripts/fishDataProvider.js
@@ -0,0 +1,20 @@
+const fishCollection = [
+    {name: "Nemo",
+     species: "Clownfish",
+     length: "5in",
+     location: "Great Barrier Reef",
+     diet: "Plantkton"
+     image: "images/download.jpeg"
+    }
+    {name: "Dory",
+     species: "Royal Blue Tang",
+     length: "10in",
+     location: "American Samoa",
+     diet: "Algae"
+     image: "images/blue-tang.jpg"
+    }
+]
+
+Export const useFish = () => {
+    return fishCollection.slice()
+}
---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+import { useFish } from "./scripts/fishDataProvider.js"
+
+const allTheFish = useFish();
+
+for (const fishObj of allTheFish){
+    
+}
